672336911bb30e20b5540e08930ee6af2542a264
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-05-14 23:03:12 +0700

    introduction web services

---
+++ ASPNetWebService/CalculatorWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ASPNetWebService
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
---
+++ ASPNetWebService/Readme.txt
@@ -0,0 +1,5 @@
+﻿- Web services are a standard way for developing interoperator applications.
+- These applications can be on the same computer or different computers.
+- Web services use protocols like HTTP, SOAP and XML.
+- HTTP is the protocol used by web services to send and receive messages.
+- The message protocol is SOAP messages are in XML format.
