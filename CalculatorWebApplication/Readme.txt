2c8ff4e6ef657c6e3ac1ae6032aa0efed9ebe3bf
Wanasak Suraintaranggoon <u510610433@gmail.com>
2017-05-14 23:40:45 +0700

    consuming web service

---
+++ CalculatorWebApplication/Readme.txt
@@ -0,0 +1,14 @@
+﻿- What is WSDL and what's purpose?
+- How is proxy class generated?
+- What is the use of a proxy class?
+- What actually happens when a web service reference is added?
+
+Visual Studio generates a proxy class using the WSDL of the web service. The WSDL contains 
+1) all the methods that are exposed
+2) parameter and their types
+3) return types of the methods.
+
+Client application calls  the proxy class method.
+The proxy class will seriallize the parameters and prepare a SOAP request message and send it to the web service.
+The web service executes method and return SOAP response message to the proxy.
+The proxy class will deserialize SOAP response message and send it to client.
---
+++ CalculatorWebApplication/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            CalculatorService.CalculatorWebServiceSoapClient client = new CalculatorService.CalculatorWebServiceSoapClient();
+            int result = client.Add(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+            lblResult.Text = result.ToString();
+        }
+    }
+}
